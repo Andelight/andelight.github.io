5ad21c8032017614f2ae3a4e20da38211f981b41
andelight <Andelight.99@gmail.com>
2019-09-25 00:28:59 +0300

    buttons fix

--- testTask/js/script.js
+++ testTask/js/script.js
@@ -4,22 +4,27 @@ let docBtn = document.querySelector('.doc_button'),
 
 transBtn.addEventListener('click', () => {
   let main = document.querySelector('.main');
-    main.style.display = 'none';
-  
+  main.style.display = 'none';
+  transBtn.style.borderBottom = "3px solid #87d4f0";
+  docBtn.style.borderBottom = 'none';
+
   if (flag === false) {
     emptyWindowCreating();
   }
+
 });
 
 docBtn.addEventListener('click', () => {
   let main = document.querySelector('.main');
   main.style.display = 'block';
+  docBtn.style.borderBottom = "3px solid #87d4f0";
+  transBtn.style.borderBottom = 'none';
 });
 
 
 function emptyWindowCreating () {
   flag = true;
-  
+
   let window = document.createElement('div');
   window.innerText = "Список транзакций пуст!";
 
