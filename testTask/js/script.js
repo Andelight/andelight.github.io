2fe997788089b0a5866d15dd42b2bc9fed38058e
andelight <Andelight.99@gmail.com>
2019-09-25 09:36:11 +0300

    script fix

--- testTask/js/script.js
+++ testTask/js/script.js
@@ -1,33 +1,37 @@
-let docBtn = document.querySelector('.doc_button'),
+window.addEventListener('DOMContentLoaded', function() {
+  let docBtn = document.querySelector('.doc_button'),
     transBtn = document.querySelector('.transaction_button'),
     flag = false;
 
-transBtn.addEventListener('click', () => {
-  let main = document.querySelector('.main');
-  main.style.display = 'none';
-  transBtn.style.borderBottom = "3px solid #87d4f0";
-  docBtn.style.borderBottom = 'none';
+  transBtn.addEventListener('click', () => {
+    let main = document.querySelector('.main');
+    main.style.display = 'none';
+    transBtn.style.borderBottom = "3px solid #87d4f0";
+    docBtn.style.borderBottom = 'none';
+    
+    if (flag === false) {
+      emptyWindowCreating();
+    }
+  });
 
-  if (flag === false) {
-    emptyWindowCreating();
+  let windowCl = document.createElement('div');
+
+  docBtn.addEventListener('click', () => {
+    let main = document.querySelector('.main');
+    main.style.display = 'block';
+    docBtn.style.borderBottom = "3px solid #87d4f0";
+    transBtn.style.borderBottom = 'none';
+
+    if (windowCl != null) windowCl.remove();
+    flag = false;
+  });
+
+
+  function emptyWindowCreating () {
+    flag = true;
+    windowCl.innerText = "Список транзакций пуст!";
+
+    windowCl.classList.add('window');
+    document.body.appendChild(windowCl);
   }
-
-});
-
-docBtn.addEventListener('click', () => {
-  let main = document.querySelector('.main');
-  main.style.display = 'block';
-  docBtn.style.borderBottom = "3px solid #87d4f0";
-  transBtn.style.borderBottom = 'none';
-});
-
-
-function emptyWindowCreating () {
-  flag = true;
-
-  let window = document.createElement('div');
-  window.innerText = "Список транзакций пуст!";
-
-  window.classList.add('window');
-  document.body.appendChild(window);
-}
+})
